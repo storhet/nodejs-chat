53916738697ad1425b771f8fbf5d930b225118f8
Kris Birov <kris.birov@gmail.com>
2015-02-24 00:23:29 +0200

    add openshift ip, port

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var colors = ['red', 'green', 'blue', 'yellow', 'orange'];
 
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
 io.on('connection', function(client) {
   client.on('join', function(name) {
     console.log(name + ' joined the chat!');
@@ -11,25 +14,23 @@ io.on('connection', function(client) {
     client.color = colors[Math.floor((Math.random() * 5))];
   });
 
-	client.on('messages', function(message) {
+  client.on('messages', function(message) {
     var nickname = client.nickname;
     var color = client.color;
-    client.broadcast.emit('messages', {
-      name: nickname,
-      message: message,
-      color: color
-    });
 
-    client.emit('messages', {
+    var message = {
       name: nickname,
       message: message,
       color: color
-    });
-	});
+    };
+
+    client.broadcast.emit('messages', message);
+    client.emit('messages', message);
+  });
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 })
 
-server.listen(8080);
+server.listen(port, ipaddr);
